6680a7e6741e28c65f17966ada14c9bbc7747611
lanzhi6 <lan_zhi@outlook.com>
2022-02-26 15:57:10 +0800

    nul

--- src/main/java/me/lanzhi/bluestarapi/Api/yamlConfig.java
+++ src/main/java/me/lanzhi/bluestarapi/Api/yamlConfig.java
@@ -36,6 +36,30 @@ public class yamlConfig extends YamlConfiguration
         }
     }
 
+    public void reload()
+    {
+        try
+        {
+            file.createNewFile();
+        }
+        catch (IOException e)
+        {
+            System.out.println("§4[BluestarAPI]创建文件时出错:"+file.getName());
+        }
+        try
+        {
+            this.load(file);
+        }
+        catch (IOException e)
+        {
+            System.out.println("§4[BluestarAPI]加载文件时出错:" + file.getName());
+        }
+        catch (org.bukkit.configuration.InvalidConfigurationException e)
+        {
+            System.out.println("§4[BluestarAPI]加载文件时出错:" + file.getName());
+        }
+    }
+
     public void save()
     {
         try
