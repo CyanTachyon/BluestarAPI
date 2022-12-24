42f3cf3e2bd0fe07fd77e66bc75b53b6444c3fcc
lanzhi6 <lan_zhi@outlook.com>
2022-12-24 12:23:04 +0800

    [3.3.0]

--- BluestarAPI-Bukkit-injector/src/main/java/me/lanzhi/api/config/AutoSerializeInterface.java
+++ BluestarAPI-Bukkit-injector/src/main/java/me/lanzhi/api/config/AutoSerializeInterface.java
@@ -9,12 +9,28 @@ import java.util.Map;
  */
 public interface AutoSerializeInterface extends ConfigurationSerializable
 {
+    public static void registerClass(Class<? extends AutoSerializeInterface> clazz)
+    {
+        AutoSerialize.registerClass(clazz);
+    }
+
+    public static AutoSerializeInterface deserialize(Map<String,Object> map)
+    {
+        return AutoSerialize.deserialize(map);
+    }
+
+    public static Map<String,Object> serialize(AutoSerializeInterface object)
+    {
+        return AutoSerialize.serialize(object);
+    }
+
     /**
      * 序列化,如果你重新了该方法,那所有自动序列化功能都不会生效
+     *
      * @return 序列化结果
      */
     @Override
-    default Map<String, Object> serialize()
+    default Map<String,Object> serialize()
     {
         Class<? extends AutoSerializeInterface> clazz=this.getClass();
         AutoSerialize.registerClass(clazz);
